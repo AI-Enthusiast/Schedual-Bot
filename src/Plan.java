72fb644c82b807a388b8cc0c7f0e99d6c1d93d03
Cormac <Rareear97>
2017-04-29 20:11:19 -0400

    A bot to carry the brunt of the work in course planning for college

---
+++ src/Plan.java
@@ -0,0 +1,11 @@
+/**
+ * A class to determine what classes to pick
+ * @author Cormac
+ * @since 4/25/17
+ */
+public class Plan {
+    /* Things this class should contain:
+     *
+     *
+     */
+}
---
+++ src/Semester.java
@@ -0,0 +1,15 @@
+/**
+ * A class to represent a semester
+ * @author Cormac
+ * @since 4/25/17
+ */
+public class Semester {
+
+    CourseData couseLoad[];
+
+    public Semester(CourseData[] courses){
+        this.couseLoad = courses;
+    }
+
+
+}
